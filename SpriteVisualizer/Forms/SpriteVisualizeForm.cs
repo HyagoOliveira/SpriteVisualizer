f7ea8d105d8a050add6afaaaf1636ebab59a2765
Hyago Oliveira <hyago.oliveira@tapps.games>
2020-08-28 18:04:40 -0300

    Add default Strip Menu

--- SpriteVisualizer/Forms/SpriteVisualizeForm.cs
+++ SpriteVisualizer/Forms/SpriteVisualizeForm.cs
@@ -1,20 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SpriteVisualizer
 {
-    public partial class Form1 : System.Windows.Forms.Form
+    public partial class SpriteVisualizerForm : Form
     {
-        public Form1()
+        public SpriteVisualizerForm()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (var reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
--- SpriteVisualizer/Program.cs
+++ SpriteVisualizer/Program.cs
@@ -16,7 +16,7 @@ namespace SpriteVisualizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SpriteVisualizerForm());
         }
     }
 }
